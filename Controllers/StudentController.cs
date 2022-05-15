3bf80723f084cebab40dc9d2030923a7aa28d484
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-05-15 17:00:53 +0100

    added code to post in student controller

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -24,6 +24,8 @@ namespace RestAPI.Controllers
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Student), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetById(int id)
         {
             var target = await _context.Students.FindAsync(id);
@@ -35,6 +37,16 @@ namespace RestAPI.Controllers
             {
                 return Ok(target);
             }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create(Student student)
+        {
+            await _context.Students.AddAsync(student);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new { id = student.Id }, student);
         }
     }
 }
