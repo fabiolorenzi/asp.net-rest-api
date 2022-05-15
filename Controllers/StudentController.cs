54bcb4d2348086680f2c114cc6b159f6f5b1c454
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-05-15 16:49:12 +0100

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestAPI.Models;
+using RestAPI.Data;
+
+namespace RestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        public readonly StudentDbContext _context;
+
+        public StudentController(StudentDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Student>> Get()
+        {
+            return await _context.Students.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var target = await _context.Students.FindAsync(id);
+            if (target == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(target);
+            }
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace RestAPI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ClassRoom { get; set; }
+        public int Position { get; set; }
+    }
+}
